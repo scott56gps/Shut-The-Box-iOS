18a42f91d0c4fd54ebe1eb075aa64e47f204a4bb
Scott Nicholes <scott93gps@gmail.com>
2025-04-24 15:56:28 -0700

    Initial Commit

---
+++ Shut The Box/Shut_The_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shut_The_BoxApp.swift
+//  Shut The Box
+//
+//  Created by Scott Nicholes on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Shut_The_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shut The BoxTests/Shut_The_BoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  Shut_The_BoxTests.swift
+//  Shut The BoxTests
+//
+//  Created by Scott Nicholes on 4/24/25.
+//
+
+import Testing
+@testable import Shut_The_Box
+
+struct Shut_The_BoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
